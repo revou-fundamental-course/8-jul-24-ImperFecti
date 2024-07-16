18e143d98c1816d18d905439fcb3afdaedf35150
ImperFecti <adilm8909@gmail.com>
2024-07-16 12:59:58 +0700

    pembuatan desain

--- js/script.js
+++ js/script.js
@@ -5,24 +5,35 @@ document.getElementById("bmiForm").addEventListener("submit", function (e) {
   const height = parseFloat(document.getElementById("height").value) / 100;
 
   if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
-    alert("Please enter valid values for weight and height.");
+    alert("Masukkan nilai yang valid untuk berat dan tinggi badan.");
     return;
   }
 
   const bmi = (weight / (height * height)).toFixed(2);
   let category = "";
+  let explanation = "";
 
   if (bmi < 18.5) {
-    category = "Underweight";
+    category = "Kekurangan Berat Badan";
+    explanation =
+      "BMI Anda menunjukkan bahwa Anda kekurangan berat badan. Pertimbangkan untuk berkonsultasi dengan penyedia layanan kesehatan untuk menentukan kemungkinan penyebab dan strategi penambahan berat badan yang sehat.";
   } else if (bmi < 24.9) {
-    category = "Normal weight";
+    category = "Berat Badan Normal";
+    explanation =
+      "BMI Anda berada dalam kisaran normal. Pertahankan pola makan yang seimbang dan aktivitas fisik yang teratur untuk menjaga BMI Anda dalam kisaran ini.";
   } else if (bmi < 29.9) {
-    category = "Overweight";
+    category = "Berat Badan Berlebih";
+    explanation =
+      "BMI Anda menunjukkan bahwa Anda kelebihan berat badan. Pertimbangkan untuk menerapkan pola makan yang lebih sehat dan meningkatkan aktivitas fisik untuk mengurangi berat badan Anda.";
   } else {
-    category = "Obese";
+    category = "Obesitas";
+    explanation =
+      "BMI Anda menunjukkan obesitas. Penting untuk berkonsultasi dengan penyedia layanan kesehatan untuk mengatasi potensi risiko kesehatan dan mengembangkan rencana pengelolaan berat badan.";
   }
 
-  document.getElementById(
-    "result"
-  ).innerHTML = `Your BMI is ${bmi} (${category})`;
+  document.getElementById("result").innerHTML = `
+        <h2>${category}</h2>
+        <p>Hasil <b>BMI</b> anda adalah ${bmi}</p>
+        <p>${explanation}</p>
+    `;
 });
